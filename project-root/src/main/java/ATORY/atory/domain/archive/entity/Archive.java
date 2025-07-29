5744b87240f937dc8f6ed354f8f77b0d5e189971
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-07-30 02:39:29 +0900

    [FEATURE] 아카이브 구조 설정

---
+++ project-root/src/main/java/ATORY/atory/domain/archive/entity/Archive.java
@@ -0,0 +1,34 @@
+package ATORY.atory.domain.archive.entity;
+
+import ATORY.atory.domain.post.entity.Post;
+import ATORY.atory.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "Archive")
+public class Archive {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Builder
+    public Archive(Long id, User user, Post post) {
+        this.id = id;
+        this.user = user;
+        this.post = post;
+    }
+}
