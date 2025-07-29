2530ac7fd45052387ebee5ccb865a8406547e627
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-07-30 02:40:41 +0900

    [FEATURE] 게시물 구조 설정

---
+++ project-root/src/main/java/ATORY/atory/domain/post/entity/Post.java
@@ -0,0 +1,44 @@
+package ATORY.atory.domain.post.entity;
+
+import ATORY.atory.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "Post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String name;
+
+    @Column(columnDefinition = "JSON", nullable = true)
+    private String imageURL;
+
+    @Column(columnDefinition = "JSON", nullable = true)
+    private String exhibitionURL;
+
+    private String description;
+    private PostType postType;
+
+    @Builder
+    public Post(Long id, User user, String name, String imageURL, String exhibitionURL, String description, PostType postType) {
+        this.id = id;
+        this.user = user;
+        this.name = name;
+        this.imageURL = imageURL;
+        this.exhibitionURL = exhibitionURL;
+        this.description = description;
+        this.postType = postType;
+    }
+}
---
+++ project-root/src/main/java/ATORY/atory/domain/post/service/PostService.java
@@ -0,0 +1,11 @@
+package ATORY.atory.domain.post.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class PostService {
+}
