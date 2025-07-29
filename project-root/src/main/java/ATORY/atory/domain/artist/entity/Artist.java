61b42e626020a4ab5cfd66e176d214863e4e7672
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-07-30 02:39:49 +0900

    [FEATURE] 청년 작가 구조 설정

---
+++ project-root/src/main/java/ATORY/atory/domain/artist/entity/Artist.java
@@ -0,0 +1,35 @@
+package ATORY.atory.domain.artist.entity;
+
+import ATORY.atory.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "Artist")
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String birth;
+    private String educationBackground;
+
+    private Boolean disclosureStatus = Boolean.TRUE;
+
+    @Builder
+    public Artist(User user, String birth, String educationBackground, Boolean disclosureStatus) {
+        this.user = user;
+        this.birth = birth;
+        this.educationBackground = educationBackground;
+        this.disclosureStatus = disclosureStatus;
+    }
+}
