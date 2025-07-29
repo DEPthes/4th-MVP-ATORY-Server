18263db4f9504d462fb1b16cf5772ab8f0438474
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-07-30 02:40:21 +0900

    [FEATURE] 팔로우 구조 설정

---
+++ project-root/src/main/java/ATORY/atory/domain/follow/service/FollowService.java
@@ -0,0 +1,11 @@
+package ATORY.atory.domain.follow.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class FollowService {
+}
