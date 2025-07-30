d359e1bc3d47f2684da3f9d237a90861ad99f9d5
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-07-30 16:33:52 +0900

    [FEATURE] PostDate 구조 설계

---
+++ project-root/src/main/java/ATORY/atory/domain/post/entity/PostDate.java
@@ -0,0 +1,35 @@
+package ATORY.atory.domain.post.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Setter
+@Service
+@Table(name = "PostDate")
+public class PostDate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Builder
+    public PostDate(Long id, LocalDateTime createdAt, LocalDateTime modifiedAt, Post post) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.post = post;
+    }
+}
