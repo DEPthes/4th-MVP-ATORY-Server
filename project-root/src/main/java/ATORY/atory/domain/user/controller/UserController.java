a3cd688598cd8530bac645c98cff267fff799752
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-08-15 00:29:34 +0900

    [FEAT] 아티스트 가입 기능 추가

--- project-root/src/main/java/ATORY/atory/domain/user/controller/UserController.java
+++ project-root/src/main/java/ATORY/atory/domain/user/controller/UserController.java
@@ -1,6 +1,15 @@
 package ATORY.atory.domain.user.controller;
 
+import ATORY.atory.domain.artist.dto.ArtistRegisterDto;
+import ATORY.atory.domain.collector.dto.CollectorRegisterDto;
+import ATORY.atory.domain.user.service.UserRegisterService;
+import ATORY.atory.global.dto.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/user")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserRegisterService userRegisterService;
+
+    @Operation(summary = "작가 회원가입", description = "작가 회원가입 api")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 확인됨"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력 값"),
+            @ApiResponse(responseCode = "500", description = "서버 오류 발생")
+    })
+    @PostMapping("/register/artist")
+    public ApiResult<?> registerArtist(@RequestBody ArtistRegisterDto artistRegisterDto) {
+        Boolean result = userRegisterService.artistRegister(artistRegisterDto);
+
+        return ApiResult.ok(result);
+    }
+
+    @Operation(summary = "콜렉터 회원가입", description = "콜렉터 회원가입 api")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 확인됨"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력 값"),
+            @ApiResponse(responseCode = "500", description = "서버 오류 발생")
+    })
+    @PostMapping("/register/collector")
+    public ApiResult<?> registerCollector(@RequestBody CollectorRegisterDto collectorRegisterDto) {
+        Boolean result = userRegisterService.collectorRegister(collectorRegisterDto);
+
+        return ApiResult.ok(result);
+    }
+
+
 }
