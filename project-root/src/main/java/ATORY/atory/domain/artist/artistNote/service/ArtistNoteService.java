52de3a1608df03b16ac63626e0757168fa5760b8
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-08-18 01:09:43 +0900

    [FEAT] 메인페이지 아티스트 조회 기능 추가

--- project-root/src/main/java/ATORY/atory/domain/artist/artistNote/service/ArtistNoteService.java
+++ project-root/src/main/java/ATORY/atory/domain/artist/artistNote/service/ArtistNoteService.java
@@ -1,12 +1,22 @@
 package ATORY.atory.domain.artist.artistNote.service;
 
+import ATORY.atory.domain.artist.artistNote.dto.ArtistNoteDto;
 import ATORY.atory.domain.artist.artistNote.entity.ArtistNote;
 import ATORY.atory.domain.artist.artistNote.repository.ArtistNoteRepository;
+import ATORY.atory.domain.artist.dto.ArtistDto;
+import ATORY.atory.domain.user.entity.User;
+import ATORY.atory.domain.user.repository.UserRepository;
+import ATORY.atory.global.dto.UserType;
+import ATORY.atory.global.exception.ErrorCode;
+import ATORY.atory.global.exception.MapperException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,8 +24,29 @@ import java.util.List;
 public class ArtistNoteService {
 
     private final ArtistNoteRepository artistNoteRepository;
+    private final UserRepository userRepository;
 
     public List<ArtistNote> getByArtistId(Long artistId){
         return artistNoteRepository.findByArtistId(artistId);
     }
+
+    //메인 페이지 작가 노트 조회
+    public Page<ArtistDto> loadArtistNotes(Pageable pageable, String googleID){
+        User currentUser = userRepository.findByGoogleID(googleID).orElseThrow(() -> new MapperException(ErrorCode.SER_NOT_FOUND));
+
+        Long userId = currentUser.getId();
+
+        return userRepository.findTopByUserTypeOrderByFollowerCountDesc(UserType.ARTIST, pageable)
+                .map(user -> new ArtistDto(
+                        user.getId(),
+                        user.getUsername(),
+                        user.getContact(),
+                        user.getIntroduction(),
+                        user.getEmail(),
+                        user.getProfileImageURL(),
+                        user.getId().equals(userId)
+                ));
+    }
+
+
 }
--- project-root/src/main/java/ATORY/atory/domain/user/repository/UserRepository.java
+++ project-root/src/main/java/ATORY/atory/domain/user/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package ATORY.atory.domain.user.repository;
 
 import ATORY.atory.domain.user.entity.User;
+import ATORY.atory.global.dto.UserType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +14,17 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByGoogleID(String googleID);
+
+    @Query("""
+        SELECT u
+        FROM User u
+        LEFT JOIN Follow f ON f.following = u
+        WHERE u.userType = :userType
+        GROUP BY u
+        ORDER BY COUNT(f.id) DESC
+        """)
+    Page<User> findTopByUserTypeOrderByFollowerCountDesc(
+            @Param("userType") UserType userType,
+            Pageable pageable
+    );
 }
