72049333cc65abbbfc7d4e291341df73776018f5
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-08-18 02:24:54 +0900

    [FEAT] 태그 조회

--- project-root/src/main/java/ATORY/atory/domain/tag/repository/TagRepository.java
+++ project-root/src/main/java/ATORY/atory/domain/tag/repository/TagRepository.java
@@ -1,8 +1,11 @@
 package ATORY.atory.domain.tag.repository;
 
+import ATORY.atory.domain.tag.dto.TagDto;
 import ATORY.atory.domain.tag.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
